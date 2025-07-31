85a64f28701887d299b6c2f49dc65ef3150025eb
Yuvasri-06 <yuvasrij6@gmail.com>
2025-07-31 16:40:21 +0530

    Done

---
+++ src/org/sample/ClassA.java
@@ -0,0 +1,28 @@
+package org.sample;
+
+public class ClassA {
+	
+	private void EmpId() {
+		System.out.println("Emp Id is  :25");
+
+	}
+	
+	private void EmpName() {
+		System.out.println("Emp Name is :sam");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		ClassA c =new ClassA();
+		
+		c.EmpId();
+		c.EmpName();
+				
+		
+		
+	
+		
+	}
+
+}
